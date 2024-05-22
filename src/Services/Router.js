c23db13df5b789a02e1a93aa57d647aac36c6b2a
ranjini243 <guptaranjani67@gmail.com>
2024-05-22 15:01:36 +0530

    changes in rourter

--- src/Services/Router.js
+++ src/Services/Router.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Login from '../component/Login/Login';
 import Forgot from '../component/Forgot/Forgot';
 import Home from '../component/Home/Home';
-import SignUp from '../component/signin/SignUp';
+import SignUp from '../component/signup/SignUp';
 
 const Routes = () => {
   return useRoutes(
